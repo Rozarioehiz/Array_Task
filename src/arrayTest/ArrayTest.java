c0014598dec6ad9bfb6bfb571ea3ec66d39a2d69
Rozarioehiz <rozarioehiz@gmail.com>
2026-09-11 04:13:15 +0100

    AverageNumbers

--- src/arrayTest/ArrayTest.java
+++ src/arrayTest/ArrayTest.java
@@ -13,7 +13,15 @@ public class ArrayTest {
         int [] expected = {1};
         int [] actual =  array.smallestNumber(numbers);
         assertArrayEquals(expected, actual);
-
-
     }
+    @Test
+    public void testForAverageOfAllArrays(){{}
+        Array array = new Array();
+        double [] numbers = {1,2,3,4,5,3,9,10};
+        double [] expected = {4.625};
+        double [] actual = array.averageNumbers(numbers);
+        assertArrayEquals(expected,actual, 0.0001);
+    }
+
+
 }
--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -12,4 +12,15 @@ public class Array {
          int[] result = {smallest};
         return result;
     }
+
+    public double[] averageNumbers(double[] numbers) {
+     double sum = numbers[0];
+     for(int count = 1; count < numbers.length; count++){
+         sum = sum + numbers[count];
+     }
+     double average = sum / numbers.length;
+     double [] result = {average};
+     return result;
+    }
+
 }
